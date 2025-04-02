a21f04040106d452305758a025bfb78956758c36
LEONE Yannick <yannick.leone@gmail.com>
2025-04-02 10:00:31 +0200

    feat: form accessibility

--- resources/views/livewire/edit-profile.blade.php
+++ resources/views/livewire/edit-profile.blade.php
@@ -2,10 +2,11 @@
     <h1 class="mb-4 text-2xl text-slate-700 font-semibold">Update your profile</h1>
 
     <form wire:submit="save" class="min-w-[30rem] flex flex-col gap-6 bg-white rounded-lg shadow p-6">
-        <div class="flex flex-col gap-2">
+        <label class="flex flex-col gap-2">
             <h3 class="font-medium text-slate-700 text-base">Username</h3>
 
             <input
+                aria-label="Username"
                 wire:model.blur="form.username"
                 @class([
                     'px-3 py-2 rounded-lg',
@@ -14,12 +15,17 @@
                 ])
                 type="text"
                 placeholder="Username"
+
+                @error('form.username')
+                    aria-invalid="true"
+                    aria-description="{{ $message }}"
+                @enderror
             >
 
             @error('form.username')
-                <p class="text-sm text-red-500">{{ $message }}</p>
+                <p class="text-sm text-red-500" aria-live="assertive">{{ $message }}</p>
             @enderror
-        </div>
+        </label>
 
         <div class="flex flex-col gap-2">
             <h3 class="font-medium text-slate-700 text-base">Bio</h3>
@@ -49,6 +55,7 @@
         x-show="$wire.show_success_indicator"
         x-transition.out.opacity.duration.2000ms
         x-effect="if ($wire.show_success_indicator) setTimeout(() => $wire.show_success_indicator = false, 3000)"
+        aria-live="polite"
         class="flex justify-end pt-4"
     >
         <div class="flex gap-2 items-center text-green-500 text-sm font-medium">
