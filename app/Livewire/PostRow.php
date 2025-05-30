f6d8ad8c5d66609c4c92ee47b38bb9b49063f5f2
LEONE Yannick <yannick.leone@gmail.com>
2025-05-30 17:13:42 +0200

    feat: Modals adding an edit modal

--- app/Livewire/PostRow.php
+++ app/Livewire/PostRow.php
@@ -2,12 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\PostForm;
 use App\Models\Post;
 use Livewire\Component;
 
 class PostRow extends Component
 {
     public Post $post;
+    public PostForm $form;
+
+    public bool $show_edit_dialog = false;
+
+    public function mount(): void
+    {
+        $this->form->setPost($this->post);
+    }
+
+    public function save(): void
+    {
+        $this->form->update();
+        $this->post->refresh();
+
+        $this->show_edit_dialog = false;
+    }
 
     public function render()
     {
